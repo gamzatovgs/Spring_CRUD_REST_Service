97bc1c803bbfdd41dac1b3f83074592c9b79ced7
Gamzat <gamzatov-gs@yandex.ru>
2021-06-15 21:22:04 +0300

    Initial commit

---
+++ src/main/java/com/gamzatovgs/spring/rest/configuration/MyWebInitializer.java
@@ -0,0 +1,23 @@
+package com.gamzatovgs.spring.rest.configuration;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Nullable
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MyConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
